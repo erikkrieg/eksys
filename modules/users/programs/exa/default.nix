3ba20553ece195967effd210004217c8494fe7b2
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-02 01:18:31 -0400

    user: tree alias now groups dirs first

--- modules/users/programs/exa/default.nix
+++ modules/users/programs/exa/default.nix
@@ -5,7 +5,7 @@
   programs.zsh.shellAliases = {
     ls = "exa --group-directories-first";
     l = lib.mkForce "exa --group-directories-first -la --git";
-    tree="exa --tree -a -I='.git'";
+    tree="exa --group-directories-first --tree -a -I='.git'";
     tree2="tree --level=2";
     tree3="tree --level=3";
     tree4="tree --level=4";
