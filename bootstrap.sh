1168c6a0f5c40137937fbb4d4cd34e7f806b5237
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-11 09:09:37 -0400

    flake: use dynamic hostname for bootsrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 nix --extra-experimental-features "nix-command flakes" \
-  build .#darwinConfigurations.Eriks-MacBook-Pro.system
+  build ".#darwinConfigurations.$(hostname -s).system"
 
 echo "First install tends to abort with error that includes manual remediation steps"
 ./result/sw/bin/darwin-rebuild switch --flake .
