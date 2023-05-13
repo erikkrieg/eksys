605d207dbd1ec94bda2884dc05b80c48095e2296
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-12 22:18:35 -0400

    user: adds a few more git aliases that I often use

--- modules/users/programs/git/default.nix
+++ modules/users/programs/git/default.nix
@@ -19,18 +19,26 @@
     };
   };
 
+  # Based on OMZ git aliases
+  # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
   programs.zsh.shellAliases = {
     g = "git";
+    gb = "git branch";
+    gbd = "git branch -d";
+    gbD = "git branch -D";
     gs = "git status";
     gd = "git diff";
     gl = "git pull";
+    ggl = "git pull origin $(git branch --show-current)";
     gp = "git push";
+    ggp = "git push origin $(git branch --show-current)";
     ga = "git add";
     gf = "git fetch";
     gco = "git checkout";
     gcb = "git checkout -b";
     gcam = "git commit -am";
     gcmsg = "git commit -m";
+    gm = "git merge";
   };
 }
 
