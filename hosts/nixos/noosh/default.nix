32bb0b8cb52613ca23403848be183817b3dbcfc1
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-20 02:23:59 -0400

    noosh: enable zsh

--- hosts/nixos/noosh/default.nix
+++ hosts/nixos/noosh/default.nix
@@ -79,6 +79,8 @@
     git
   ];
 
+  programs.zsh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
