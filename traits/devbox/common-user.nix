d36ab3cf6b507b2bef37f8baa29547b6cf29cfd6
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-20 18:30:54 -0400

    devbox: moves shellcheck into nvim module
    adds comment to explain what uses shellcheck

--- traits/devbox/common-user.nix
+++ traits/devbox/common-user.nix
@@ -34,7 +34,6 @@
     just
 
     # Language-specific
-    shellcheck
     pipenv
   ];
 
--- traits/devbox/programs/nvim/default.nix
+++ traits/devbox/programs/nvim/default.nix
@@ -1,6 +1,11 @@
 { pkgs, envim, ... }: with pkgs; {
   home.packages = [
     envim
+
+    # Used by bash-language-server (which is installed in envim).
+    # I'd rather shellcheck be packaged directly in envim, but I'm not sure how
+    # to include it in the path so that the bash ls can access it.
+    shellcheck
   ];
 
   home.sessionVariables = {
