8492b3f6ec80c62202b69b84f7a5e6d46b5c5e69
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-21 01:26:13 -0400

    server: installs k3s
    adds comment for using service.k3s in the future

--- traits/server/nixos-host.nix
+++ traits/server/nixos-host.nix
@@ -1,5 +1,17 @@
 # NixOS-specific server host configuration.
 # https://search.nixos.org/options
-{ ... }: {
+{ pkgs, ... }: with pkgs; {
   imports = [ ];
+
+  # This is to demonstrate how nixos offers support for k3s. But, I want to set this
+  # a bit more directly first and then may migrate to this when I know what I want.
+  # Ref: https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=k3s
+  # services.k3s = {
+  #   enable = true;
+  #   role = "server"; # Server also acts as an agent.
+  # };
+
+  environment.systemPackages = [
+    k3s
+  ];
 }
