d71d3106024f32770060a07afacee99750f7b956
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-24 01:55:09 -0400

    server: harden openssh security configuration

--- traits/server/nixos-host.nix
+++ traits/server/nixos-host.nix
@@ -17,4 +17,19 @@
 
 
   security.sudo.execWheelOnly = true;
+
+  services.openssh = {
+    enable = true;
+    allowSFTP = false;
+    settings = {
+      PasswordAuthentication = false;
+      X11Forwarding = false;
+    };
+    extraConfig = ''
+      AllowTcpForwarding yes
+      AllowAgentForwarding no
+      AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
+    '';
+  };
 }
