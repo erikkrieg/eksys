1d48f268d7c17bb6461d44b868f80344b2212aba
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-25 01:11:58 -0400

    attempts to create a nixops configuration to deploy noosh

--- flake.nix
+++ flake.nix
@@ -31,5 +31,39 @@
         inherit inputs nixpkgs home-manager envim;
       }
     );
+
+    nixopsConfigurations.default = let system = "x86_64-linux"; in {
+      inherit nixpkgs;
+      network = {
+        description = "testing-nixops";
+        storage.legacy = { };
+      };
+      noosh = {
+        deployment = {
+          targetHost = "192.168.1.26";
+        };
+        nixpkgs.pkgs = import nixpkgs { inherit system; };
+        #modules = [
+        imports = [
+          ./hosts/nixos/noosh
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                envim = envim.packages.${system}.default;
+              };
+              users.ek.imports = [
+                ./traits/devbox/nixos-user.nix
+                ./traits/server/nixos-user.nix
+              ];
+            };
+          }
+          ./traits/devbox/nixos-host.nix
+          ./traits/server/nixos-host.nix
+        ];
+      };
+    };
   };
 }
