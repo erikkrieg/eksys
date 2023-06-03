e317109a5f04bdeb5ba7589797f770f512050ec6
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-03 08:45:20 -0400

    june: disable gnome autosuspend

--- hosts/nixos/june/default.nix
+++ hosts/nixos/june/default.nix
@@ -15,6 +15,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
 
   nix.extraOptions = ''experimental-features = nix-command flakes'';
 
