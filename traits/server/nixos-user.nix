6ff6751d6769d15e665ed5b7603cd72f6ef609f0
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-15 09:50:43 -0400

    server: remove unneeded common-user module

--- traits/server/nixos-user.nix
+++ traits/server/nixos-user.nix
@@ -1,7 +1,3 @@
 # NixOS-specific server user configuration via home-manager.
 # https://nix-community.github.io/home-manager/options.html
-{ ... }: {
-  imports = [
-    ./common-user.nix
-  ];
-}
+{ ... }: { }
