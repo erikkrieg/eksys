2b50136acf1b7f5797abe2a4fd2545209da1ed9a
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-23 01:32:03 -0400

    adds dns trait with adguardhome

---
+++ traits/dns/nixos-host.nix
@@ -0,0 +1,24 @@
+{ ... }:
+let
+  dns_port = 53;
+  dns_ui_port = 9002;
+in
+{
+  networking.firewall = {
+    allowedTCPPorts = [ dns_port dns_ui_port ];
+    allowedUDPPorts = [ dns_port ];
+  };
+  services.adguardhome = {
+    enabled = true;
+    mutableSettings = false;
+    settings = {
+      bind_port = dns_ui_port;
+      bind_host = "0.0.0.0";
+      dns = {
+        bind_port = dns_port;
+        bind_host = [ "0.0.0.0" ];
+        bootstrap_dns = [ "1.1.1.1" "8.8.8.8" ];
+      };
+    };
+  };
+}
