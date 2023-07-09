778dd4d6142ce3cb529fb3b1b003e784825faab0
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-07-09 11:20:17 -0400

    nixos: toggle for hm was not working so removing for now

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,21 +1,21 @@
 { nixpkgs, disko, home-manager, envim, ... }@args:
 let
-  mkHost = { system, user, traits, modules ? [ ], hm_enable ? true }: (nixpkgs.lib.nixosSystem) {
+  mkHost = { system, user, traits, modules ? [ ] }: (nixpkgs.lib.nixosSystem) {
     pkgs = import nixpkgs { inherit system; };
-    modules = modules
-      ++ (nixpkgs.lib.optional hm_enable [
-      home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          extraSpecialArgs = {
-            envim = envim.packages.${system}.default;
+    modules = modules ++
+      [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              envim = envim.packages.${system}.default;
+            };
+            users.${user}.imports = map (trait: ../../traits/${trait}/nixos-user.nix) traits;
           };
-          users.${user}.imports = map (trait: ../../traits/${trait}/nixos-user.nix) traits;
-        };
-      }
-    ]) ++ map (trait: ../../traits/${trait}/nixos-host.nix) traits;
+        }
+      ] ++ map (trait: ../../traits/${trait}/nixos-host.nix) traits;
   };
 in
 {
