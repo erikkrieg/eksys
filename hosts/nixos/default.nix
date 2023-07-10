e53ce302e4c0de14906d0aacfa814f5d6a33a24f
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-07-09 20:48:00 -0400

    nixos: new_host output to demonstrate output for installing nixos with nixos-anywhere

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -40,27 +40,27 @@ in
     modules = [ disko.nixosModules.disko ./chips ];
   };
 
-  # chips = nixpkgs.lib.nixosSystem {
-  #   system = "x86_64-linux";
-  #   specialArgs = args;
-  #   modules = [
-  #     ({ modulesPath, ... }: {
-  #       imports = [
-  #         (modulesPath + "/installer/scan/not-detected.nix")
-  #         (modulesPath + "/profiles/qemu-guest.nix")
-  #         disko.nixosModules.disko
-  #         (import ./disks.nix {
-  #           disks = [ "/dev/sda" ];
-  #         })
-  #       ];
-  #       boot.loader.systemd-boot.enable = true;
-  #       boot.loader.efi.canTouchEfiVariables = true;
-  #       services.openssh.enable = true;
-  #
-  #       users.users.root.openssh.authorizedKeys.keys = [
-  #         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxcsEfj6Tnq8qJt3WFLYM4EwBYWwL4mr458vNVqDn1W ek@june"
-  #       ];
-  #     })
-  #   ];
-  # };
+  # NixOS can be installed on a new host with an output like this. I'm keeping
+  # this around for now until I figure out how to fold this into the "final" config.
+  new_host = nixpkgs.lib.nixosSystem {
+    # nixos-anywhere only works with intel chips atm
+    system = "x86_64-linux";
+    specialArgs = args;
+    modules = [
+      ({ modulesPath, ... }: {
+        imports = [
+          (modulesPath + "/installer/scan/not-detected.nix")
+          disko.nixosModules.disko
+          (import ./disks.nix { disks = [ "/dev/sda" ]; })
+        ];
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+        services.openssh.enable = true;
+        # Current config doesn't create any passwords so SSH is the only way to manage the host
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxcsEfj6Tnq8qJt3WFLYM4EwBYWwL4mr458vNVqDn1W ek@june"
+        ];
+      })
+    ];
+  };
 }
