4701c6d4ed6c704e6c433f439dc6d3cbf6b040ab
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-07-10 23:00:17 -0400

    server: open k3s server ports

--- traits/server/services/k3s/default.nix
+++ traits/server/services/k3s/default.nix
@@ -8,7 +8,16 @@
     extraFlags = ''--node-label "host=${config.networking.hostName}"'';
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  # https://docs.k3s.io/installation/requirements#inbound-rules-for-k3s-server-nodes
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+
+    2379 # Required for HA
+    2380 # Required for HA
+    6443 # K3s supervisor and Kubernetes API Server
+    10250 # Kubelet metrics
+  ];
 
   environment.sessionVariables = {
     KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
