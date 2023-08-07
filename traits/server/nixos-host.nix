cd6a5df5bde1b56f0e277263c620ced17e0a7179
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-08-07 12:52:23 -0400

    fix tailscale service and advertise additional node features

--- traits/server/nixos-host.nix
+++ traits/server/nixos-host.nix
@@ -1,8 +1,9 @@
 # NixOS-specific server host configuration.
 # https://search.nixos.org/options
-{ config, pkgs, ... }: with pkgs; {
+{ pkgs, ... }: with pkgs; {
   imports = [
     ./services/k3s
+    ./services/tailscale
   ];
 
   security.sudo.execWheelOnly = true;
@@ -18,27 +19,6 @@
       AllowAgentForwarding no
       AllowStreamLocalForwarding no
       AuthenticationMethods publickey
-    '';
-  };
-
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "server";
-  };
-
-  systemd.services.tailscale-up = {
-    after = [ "tailscale.service" "k3s.service" ];
-    wants = [ "tailscale.service" "k3s.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-    };
-    script = ''
-      status=$(${config.systemd.package}/bin/systemctl show -P StatusText tailscaled.service)
-      if [[ $status != Connected* ]]; then
-        auth="$(${kubectl}/bin/kubectl exec deploy/headscale -n headscale -- headscale authkeys create -u server --expiration 5m)"
-        ${tailscale}/bin/tailscale up --auth-key $auth --login-server=http://127.0.0.1:30080
-      fi
     '';
   };
 
