1b3c354582542da4b0f6de73989be5456a849df9
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-08-17 23:59:23 -0400

    migrates homebrew casks from devbox to guibox

--- traits/devbox/darwin-host.nix
+++ traits/devbox/darwin-host.nix
@@ -52,44 +52,11 @@
     remapCapsLockToEscape = true;
   };
 
-  # Configure Finder 
-  system.defaults = {
-    finder.AppleShowAllExtensions = true;
-    NSGlobalDomain.AppleShowAllExtensions = true;
-  };
-
-  # Homebrew is included to install packages that are missing from nixpkgs
-  homebrew = {
-    enable = true;
-    caskArgs.no_quarantine = true;
-    global.brewfile = true;
-    casks = [
-      "android-file-transfer"
-      "brave-browser"
-      "spotify"
-      "discord"
-      "balenaetcher"
-      "obsidian"
-      "sigmaos"
-    ];
-    onActivation = {
-      cleanup = "zap";
-      upgrade = true;
-      autoUpdate = true;
-    };
-  };
-
-  environment.systemPath = [ "/opt/homebrew/bin" ];
-
   environment.systemPackages = [
     wireguard-go
   ];
 
   services = {
-    yabai = {
-      # Interested in trying this out, but going to need to spike on it a bit first.
-      enable = false;
-    };
     tailscale = {
       enable = true;
       # Set nameserver to 100.100.100.100
--- traits/guibox/darwin-host.nix
+++ traits/guibox/darwin-host.nix
@@ -1,7 +1,38 @@
 { ... }: {
+  # Configure Finder 
+  system.defaults = {
+    finder.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+  };
+
+  # Homebrew is included to install packages that are missing from nixpkgs
   homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
     casks = [
+      "android-file-transfer"
+      "brave-browser"
+      "spotify"
+      "discord"
+      "balenaetcher"
+      "obsidian"
+      "sigmaos"
       "slack"
     ];
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+      autoUpdate = true;
+    };
+  };
+
+  environment.systemPath = [ "/opt/homebrew/bin" ];
+
+  services = {
+    yabai = {
+      # Interested in trying this out, but going to need to spike on it a bit first.
+      enable = false;
+    };
   };
 }
