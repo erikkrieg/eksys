a56629e62d313eb644696962df21b4a8ae0cbc69
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-09-20 20:35:40 -0400

    fixes addition of argocd

--- hosts/darwin/ek_pro/default.nix
+++ hosts/darwin/ek_pro/default.nix
@@ -6,7 +6,7 @@
     ];
   };
 
-  home.packages = [
+  environment.systemPackages = [
     argocd
   ];
 
--- hosts/darwin/eksys_pro/default.nix
+++ hosts/darwin/eksys_pro/default.nix
@@ -9,7 +9,7 @@
     ];
   };
 
-  home.packages = [
+  environment.systemPackages = [
     argocd
   ];
 
