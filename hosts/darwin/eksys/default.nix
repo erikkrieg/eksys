8b6ccf144a55bcb18a08a80699294acfe2babe06
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-01-20 17:34:23 -0500

    try to manage eksys host nix version through nix-darwin

--- hosts/darwin/eksys/default.nix
+++ hosts/darwin/eksys/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   homebrew = {
     casks = [
       "obs" # video recording and live streaming
@@ -6,6 +6,9 @@
       "appflowy" # Notion-like app that I'm testing
     ];
   };
+
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
 
   # Disabling until I'm ready to commit to replacing wg with tailscale
   # services.tailscale.overrideLocalDns = true;
