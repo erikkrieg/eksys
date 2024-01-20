41dd8bfbcb2c4c9eccb2316c7acdda7dc5652799
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-01-20 18:07:23 -0500

    upgrade nix to 2.14.*

--- hosts/darwin/eksys/default.nix
+++ hosts/darwin/eksys/default.nix
@@ -7,8 +7,8 @@
     ];
   };
 
+  nix.package = pkgs.nixVersions.nix_2_14;
   services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
 
   # Disabling until I'm ready to commit to replacing wg with tailscale
   # services.tailscale.overrideLocalDns = true;
