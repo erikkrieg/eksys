e0bd39b1d09fd39e21451144df92123f9178e8aa
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-11-15 22:23:39 -0500

    adds nixos host, june, pub key to minimal iso

--- iso/minimal.nix
+++ iso/minimal.nix
@@ -2,6 +2,7 @@
   imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
   users.users.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjh1wU4Pqef3+Buo/nFgsdQlbLaRAiz7L6tX+n3fJ74 ek@eksys"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxcsEfj6Tnq8qJt3WFLYM4EwBYWwL4mr458vNVqDn1W ek@june"
   ];
   services.openssh = {
     enable = true;
