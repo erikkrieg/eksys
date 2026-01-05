37402a7d4f3132d1c02deb4bdeebc47cdb18f071
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2026-01-05 16:22:30 -0500

    chore: put username in prompts instead of host (not sure if I will keep this)

--- traits/devbox/programs/zsh/config/prompt.zsh
+++ traits/devbox/programs/zsh/config/prompt.zsh
@@ -14,11 +14,12 @@ typeset -A C
 set -A C ${(kv)SPRING_COLOR}
 
 P_HOST='%F{133}$(hostname -s) %f'
+P_USER='%F{133}$(whoami) %f'
 P_VIM_MODE='%F{$C[vi]}${ZVM_MODE}%f '
 P_DIR='%F{$C[dir]}%1~%f '
 P_GIT='%F{$C[git]}$(prompt_git)%f'
 P_CARET='%F{$C[caret]}❯%f '
 
 # Use $P_PREFIX to add custom text to start of prompt.
-PROMPT="${P_PREFIX}${P_VIM_MODE}${P_HOST} ${P_DIR}${P_GIT}${P_CARET}"
+PROMPT="${P_PREFIX}${P_VIM_MODE}${P_USER} ${P_DIR}${P_GIT}${P_CARET}"
 
