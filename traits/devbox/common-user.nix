aa7252a197b1e3848d328354c09ba22a8ea8e16b
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2026-04-23 09:00:16 -0400

    fix: bk cli version that can auth login when built from nix

--- traits/devbox/common-user.nix
+++ traits/devbox/common-user.nix
@@ -1,6 +1,26 @@
 # NixOS or Darwin devbox user configuration via home-manager.
 # https://nix-community.github.io/home-manager/options.html
-{ pkgs, unstable_pkgs, llm_agents, ... }: with pkgs; {
+{ pkgs, unstable_pkgs, llm_agents, ... }: with pkgs;
+let
+  buildkite-cli = unstable_pkgs.buildkite-cli.overrideAttrs (old: rec {
+    version = "3.36.0";
+    src = fetchFromGitHub {
+      owner = "buildkite";
+      repo = "cli";
+      tag = "v${version}";
+      hash = "sha256-6cr/kFHtRNeBV8kLu0GYJN0GXehtnRfvnnfPDA2+tcQ=";
+    };
+    vendorHash = "sha256-lRoMAbU4MrOKycCIM+u2JM+8LNEvdRtQm6ZJu80czUI=";
+    doCheck = false;
+    postPatch = ''
+      patchShebangs .buildkite/{release,tag,upload-packages}.sh
+    '';
+    subPackages = null;
+    postInstall = ''
+      mv $out/bin/cli $out/bin/bk
+    '';
+  });
+in {
   # Backwards compatibility. Don't change.
   home.stateVersion = "22.11";
 
@@ -35,7 +55,7 @@
     nix-tree
     graphviz # used in combination with nix-du
     unstable_pkgs.nix-du
-    unstable_pkgs.buildkite-cli
+    buildkite-cli
 
     # Network utilities
     dig
